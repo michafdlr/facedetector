3979f8af16503a0bd91cc2057242c71e3f43d24b
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-05-21 12:49:07 +0200

    components created

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,31 @@ import Navigation from './components/navigation/Navigation'
 import Logo from './components/logo/Logo'
 import LinkInput from './components/linkInput/LinkInput'
 import Rank from './components/rank/Rank'
+import ImageDetector from './components/ImageDetector/ImageDetector'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'//
 import ParticlesBg from 'particles-bg'
+// import process from 'process';
 
 function App() {
+  const [url, setUrl] = useState('')
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    setUrl(e.target[0].value)
+    console.log(url)
+  }
+
   return (
     <>
         <Logo />
         <Navigation />
         <Rank />
-        <LinkInput />
+        <LinkInput onSubmit={handleOnSubmit} />
+        <ImageDetector url={url}/>
       {/*
-      <Login />
-      <Imagedetector /> */}
+      <Login /> */}
         <ParticlesBg type="cobweb" color="#005eff" num={300}  bg={true} />
     </>
   )
--- src/components/linkInput/LinkInput.jsx
+++ src/components/linkInput/LinkInput.jsx
@@ -1,14 +1,17 @@
-const LinkInput = () => {
+/* eslint-disable react/prop-types */
+const LinkInput = ({onSubmit}) => {
   return (
     <div className="linkbox">
       <p style={{textAlign: 'center', fontWeight: 'bold'}}>
         Put in the url to an image and detect faces
       </p>
       <div className="center">
-        <input style = {{width:'60%', height:'30px', textAlign: 'left', marginRight: '20px'}} type="text" />
-        <button>
-          Detect
-        </button>
+        <form onSubmit={onSubmit} style={{width:'60%', height:'30px', textAlign: 'left', marginRight: '20px'}}>
+          <input style={{width:'60%', height:'30px', textAlign: 'left', marginRight: '20px'}} type="text"/>
+          <button>
+            Detect
+          </button>
+        </form>
       </div>
     </div>
   )
