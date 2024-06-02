b7fc730e9d4ee70cc597bdff659c6209d2d5e119
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-06-02 20:39:40 +0200

    moved API request to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState, useLayoutEffect } from 'react'
+import { useEffect, useState, useRef} from 'react'
 import Navigation from './components/navigation/Navigation'
 import Logo from './components/logo/Logo'
 import LinkInput from './components/linkInput/LinkInput'
@@ -18,7 +18,7 @@ import Register from './components/Register/Register';
 // import viteLogo from '/vite.svg'
 
 function App() {
-  const PAT = import.meta.env.VITE_APP_PAT;
+  // const PAT = import.meta.env.VITE_APP_PAT;
   // Specify the correct user_id/app_id pairings
   // Since you're making inferences outside your app's scope
   const USER_ID = 'clarifai';
@@ -27,33 +27,37 @@ function App() {
   const MODEL_ID = 'face-detection';
   const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
 
+  const initialUser = {
+    id: '',
+    name: '',
+    email: '',
+    counter: 0,
+    joined: ''
+  }
+
 
 
   const [url, setUrl] = useState('')
   const [boxes, setBoxes] = useState([])
   const [route, setRoute] = useState('login')
   const [isSignedIn, setIsSignedIn] = useState(false)
-  const [user, setUser] = useState({
-    id: '',
-    name: '',
-    email: '',
-    counter: 0,
-    joined: ''
-  })
+  const [user, setUser] = useState(initialUser)
+  const prevUrlRef = useRef();
 
   const handleOnSubmit = (e) => {
     e.preventDefault()
-    setBoxes([])
-    setUrl(e.target[0].value)
-    fetch("http://localhost:8080/image", {
-      "method": "put",
-      "headers": {"Content-Type": "application/json"},
-      "body": JSON.stringify({
-        id: user.id
+    if (prevUrlRef.current != e.target[0].value) {
+      setUrl(e.target[0].value)
+      fetch("http://localhost:8080/image", {
+        "method": "put",
+        "headers": {"Content-Type": "application/json"},
+        "body": JSON.stringify({
+          id: user.id
+        })
       })
-    })
-      .then(response => response.json())
-      .then(counter => Object.assign(user, {"counter": counter}))
+        .then(response => response.json())
+        .then(counter => Object.assign(user, {"counter": counter}))
+    }
   }
 
   const handleOnChangeRoute = (input) => {
@@ -62,6 +66,7 @@ function App() {
     if (input === 'home') {
       setIsSignedIn(true)
     } else if (input === 'login') {
+      setUser(initialUser)
       setIsSignedIn(false)
     }
   }
@@ -76,65 +81,82 @@ function App() {
     })
   }
 
-  useLayoutEffect(() => {
-    fetch("http://localhost:8080")
-      .then(response => response.json())
-      .then(console.log)
-  }, [])
+  // useLayoutEffect(() => {
+  //   fetch("http://localhost:8080")
+  //     .then(response => response.json())
+  //     .then(console.log)
+  // }, [])
 
   useEffect(() => {
-    if (url.length != 0) {
-    const raw = JSON.stringify({
-      "user_app_id": {
-          "user_id": USER_ID,
-          "app_id": APP_ID
-      },
-      "inputs": [
-          {
-              "data": {
-                  "image": {
-                      "url": url
-                  }
-              }
-          }
-      ]
-    });
+    if (url.length != 0 && url !== prevUrlRef.current) {
+      setBoxes([])
+      const raw = JSON.stringify({
+        "user_app_id": {
+            user_id: USER_ID,
+            app_id: APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": url
+                    }
+                }
+            }
+        ]
+      });
 
     const requestOptions = {
         method: 'POST',
         headers: {
-            'Accept': 'application/json',
-            'Authorization': 'Key ' + PAT
+            // 'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            // 'Authorization': 'Key ' + PAT
         },
-        body: raw
+        // body: raw
+        body: JSON.stringify({ url: `https://api.clarifai.com/v2/models/${MODEL_ID}/versions/${MODEL_VERSION_ID}/outputs`, data: raw })
     };
-
-    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+    fetch('http://localhost:8080/api/proxy', requestOptions)
         .then(response => response.json())
         .then(result => {
-            const regions = result.outputs[0].data.regions;
-            const locBoxes = [];
-            regions.forEach(region => {
-                // Accessing and rounding the bounding box values
-                const boundingBox = region.region_info.bounding_box;
-                const topRow = boundingBox.top_row.toFixed(3);
-                const leftCol = boundingBox.left_col.toFixed(3);
-                const bottomRow = boundingBox.bottom_row.toFixed(3);
-                const rightCol = boundingBox.right_col.toFixed(3);
-                locBoxes.push([topRow*100, leftCol*100, 100 - 100*bottomRow, 100-100*rightCol])
-                // region.data.concepts.forEach(concept => {
-                //     // Accessing and rounding the concept value
-                //     const name = concept.name;
-                //     const value = concept.value.toFixed(4);
-                //     console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
-
-                // });
-            })
-            setBoxes(locBoxes);
+          const regions = result.outputs[0].data.regions;
+          const locBoxes = [];
+          regions.forEach(region => {
+              // Accessing and rounding the bounding box values
+              const boundingBox = region.region_info.bounding_box;
+              const topRow = boundingBox.top_row.toFixed(3);
+              const leftCol = boundingBox.left_col.toFixed(3);
+              const bottomRow = boundingBox.bottom_row.toFixed(3);
+              const rightCol = boundingBox.right_col.toFixed(3);
+              locBoxes.push([topRow*100, leftCol*100, 100 - 100*bottomRow, 100-100*rightCol])
+          })
+          setBoxes(locBoxes);
         })
         .catch(error => console.log('error', error));
+        prevUrlRef.current = url;
     }
   }, [url])
+
+  //   fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+  //       .then(response => response.json())
+  //       .then(result => {
+  //           const regions = result.outputs[0].data.regions;
+  //           const locBoxes = [];
+  //           regions.forEach(region => {
+  //               // Accessing and rounding the bounding box values
+  //               const boundingBox = region.region_info.bounding_box;
+  //               const topRow = boundingBox.top_row.toFixed(3);
+  //               const leftCol = boundingBox.left_col.toFixed(3);
+  //               const bottomRow = boundingBox.bottom_row.toFixed(3);
+  //               const rightCol = boundingBox.right_col.toFixed(3);
+  //               locBoxes.push([topRow*100, leftCol*100, 100 - 100*bottomRow, 100-100*rightCol])
+  //           })
+  //           setBoxes(locBoxes);
+  //       })
+  //       .catch(error => console.log('error', error));
+  //       prevUrlRef.current = url;
+  //   }
+  // }, [url])
 
   return (
     <>
